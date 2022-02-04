2b4f0533135eff5a44c4a1b02c60fffa17e6a6dc
MsingathiM <MsingathiM@users.noreply.github.com>
2022-02-04 09:56:42 +0000

    deploy: 50bc140c0ddab7d500953bfe3b92302b60d9d030

--- report.js
+++ report.js
@@ -38,28 +38,28 @@ formatter.match({
   "location": "TestDogbreeds.iOpenTheDogApiEndpoint()"
 });
 formatter.result({
-  "duration": 420741885,
+  "duration": 403678961,
   "status": "passed"
 });
 formatter.match({
   "location": "TestDogbreeds.iSearchForANonExistingDogBreedAnErrorMustBeReturned()"
 });
 formatter.result({
-  "duration": 4102779588,
+  "duration": 3560615135,
   "status": "passed"
 });
 formatter.match({
   "location": "TestDogbreeds.iVerifyThatABulldogExistFromTheListOfAllDogs()"
 });
 formatter.result({
-  "duration": 1539900660,
+  "duration": 1495563345,
   "status": "passed"
 });
 formatter.match({
   "location": "TestDogbreeds.iRetrieveAllSubBreedsForBulldogsAndTheirRespectiveImages()"
 });
 formatter.result({
-  "duration": 343057056,
+  "duration": 334819365,
   "status": "passed"
 });
 formatter.uri("getAllPets.feature");
@@ -102,14 +102,14 @@ formatter.match({
   "location": "TestPetStore.iOpenTheSwaggerEndpoint()"
 });
 formatter.result({
-  "duration": 89798,
+  "duration": 97201,
   "status": "passed"
 });
 formatter.match({
   "location": "TestPetStore.iSearchForAllPetsAvailableWithANameAndId()"
 });
 formatter.result({
-  "duration": 383425139,
+  "duration": 392157630,
   "error_message": "java.lang.AssertionError: 1 expectation failed.\nJSON path name doesn\u0027t match.\nExpected: doggie\n  Actual: Fido\n\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:490)\n\tat org.codehaus.groovy.reflection.CachedConstructor.invoke(CachedConstructor.java:72)\n\tat org.codehaus.groovy.reflection.CachedConstructor.doConstructorInvoke(CachedConstructor.java:59)\n\tat org.codehaus.groovy.runtime.callsite.ConstructorSite$ConstructorSiteNoUnwrap.callConstructor(ConstructorSite.java:84)\n\tat org.codehaus.groovy.runtime.callsite.CallSiteArray.defaultCallConstructor(CallSiteArray.java:59)\n\tat org.codehaus.groovy.runtime.callsite.AbstractCallSite.callConstructor(AbstractCallSite.java:263)\n\tat org.codehaus.groovy.runtime.callsite.AbstractCallSite.callConstructor(AbstractCallSite.java:277)\n\tat io.restassured.internal.ResponseSpecificationImpl$HamcrestAssertionClosure.validate(ResponseSpecificationImpl.groovy:499)\n\tat io.restassured.internal.ResponseSpecificationImpl$HamcrestAssertionClosure$validate$1.call(Unknown Source)\n\tat io.restassured.internal.ResponseSpecificationImpl.validateResponseIfRequired(ResponseSpecificationImpl.groovy:684)\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\n\tat java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\n\tat java.base/java.lang.reflect.Method.invoke(Method.java:566)\n\tat org.codehaus.groovy.runtime.callsite.PlainObjectMetaMethodSite.doInvoke(PlainObjectMetaMethodSite.java:43)\n\tat org.codehaus.groovy.runtime.callsite.PogoMetaMethodSite$PogoCachedMethodSiteNoUnwrapNoCoerce.invoke(PogoMetaMethodSite.java:193)\n\tat org.codehaus.groovy.runtime.callsite.PogoMetaMethodSite.callCurrent(PogoMetaMethodSite.java:61)\n\tat org.codehaus.groovy.runtime.callsite.AbstractCallSite.callCurrent(AbstractCallSite.java:185)\n\tat io.restassured.internal.ResponseSpecificationImpl.body(ResponseSpecificationImpl.groovy:262)\n\tat io.restassured.specification.ResponseSpecification$body$1.callCurrent(Unknown Source)\n\tat org.codehaus.groovy.runtime.callsite.CallSiteArray.defaultCallCurrent(CallSiteArray.java:51)\n\tat io.restassured.specification.ResponseSpecification$body$1.callCurrent(Unknown Source)\n\tat io.restassured.internal.ResponseSpecificationImpl.body(ResponseSpecificationImpl.groovy:109)\n\tat io.restassured.internal.ValidatableResponseOptionsImpl.body(ValidatableResponseOptionsImpl.java:244)\n\tat io.restassured.internal.ValidatableResponseImpl.super$2$body(ValidatableResponseImpl.groovy)\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\n\tat java.base/jdk.internal.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\n\tat java.base/jdk.internal.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\n\tat java.base/java.lang.reflect.Method.invoke(Method.java:566)\n\tat org.codehaus.groovy.reflection.CachedMethod.invoke(CachedMethod.java:107)\n\tat groovy.lang.MetaMethod.doMethodInvoke(MetaMethod.java:323)\n\tat groovy.lang.MetaClassImpl.invokeMethod(MetaClassImpl.java:1268)\n\tat org.codehaus.groovy.runtime.ScriptBytecodeAdapter.invokeMethodOnSuperN(ScriptBytecodeAdapter.java:144)\n\tat io.restassured.internal.ValidatableResponseImpl.body(ValidatableResponseImpl.groovy:298)\n\tat io.restassured.internal.ValidatableResponseImpl.body(ValidatableResponseImpl.groovy)\n\tat featureSteps.TestPetStore.iSearchForAllPetsAvailableWithANameAndId(TestPetStore.java:38)\n\tat ✽.When I search for all pets available with a name and id(getAllPets.feature:5)\n",
   "status": "failed"
 });
